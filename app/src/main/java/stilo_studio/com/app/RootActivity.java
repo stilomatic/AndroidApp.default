00cb41d756cec264528432d043d8c18ab9101ceb
Antonio Stilo <antonio.stilo@gmail.com>
2018-09-09 13:52:07 +0200

    Initial commit

---
+++ app/src/main/java/stilo_studio/com/app/RootActivity.java
@@ -0,0 +1,13 @@
+package stilo_studio.com.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RootActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_root);
+    }
+}
